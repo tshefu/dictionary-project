57eb8009fddd70f54db85fc538d9c70fe03d457c
tshefu <schneider.viktoria95@gmail.com>
2022-07-26 12:23:00 +0200

    excluded the keyword alert

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,7 +14,6 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
