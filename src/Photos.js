99c14a722ebb2a317581e9867a1303ae20a1e792
tshefu <schneider.viktoria95@gmail.com>
2022-08-04 15:06:45 +0200

    fixed alt description for img

--- src/Photos.js
+++ src/Photos.js
@@ -10,11 +10,7 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <img
-                  src={photo.src.tiny}
-                  className="img-fluid"
-                  alt="result photo"
-                />
+                <img src={photo.src.tiny} className="img-fluid" alt="result" />
               </div>
             );
           })}
